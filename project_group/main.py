126a038c095f6d98631df136c71d1f3c246693ab
Jianfenggit config --global user.email s10258730@connect.np.edu.sggit config --global --listgit config --global user.name Jianfeng <s10258730@connect.np.edu.sg>
2023-08-10 12:21:33 +0800

    main.py completed

---
+++ project_group/main.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from coh import coh_function
+from overheads import overhead_function
+from profit_loss import profitloss_function
+
+# Set file paths to COH.csv, overheads.csv, and profit_loss.csv in the current working directory.
+coh_file_path = Path.cwd()/"COH.csv"
+overheads_file_path = Path.cwd()/"overheads.csv"
+profitloss_file_path = Path.cwd() / "profit_loss.csv"
+
+
+def main():
+    """
+    -This function writes the computed amount from the overhead_function, coh_function, and profitloss_function to a text file and names it summary_report.txt.
+    """
+    # Open the summary_report.txt file for writing
+    with open('summary_report.txt', 'w') as file:
+        # File the overhead_function, coh_function, and profitloss_function results.
+        file.write(overhead_function(overheads_file_path))
+        file.write(coh_function(coh_file_path))
+        file.write(profitloss_function(profitloss_file_path))
+# Call the main function
+main()
